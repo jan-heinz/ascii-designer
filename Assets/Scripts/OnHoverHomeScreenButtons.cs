8c98f8c8eb7a73094c5810d568196270add7e08e
sharon2pan <pan.sha@northeastern.edu>
2025-10-30 15:49:24 -0400

    Add OnHoverHomeScreenButtons to scripts

---
+++ Assets/Scripts/OnHoverHomeScreenButtons.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class OnHoverHomeScreenButtons : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseOver() {
+        Debug.Log("YO IM HOVERING");
+    }
+}
