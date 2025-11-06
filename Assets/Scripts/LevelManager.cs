4cad14a4d460a2d83bf6a0bc96d2e5775d40ec73
Max Wang <44952601+max-w-uvh@users.noreply.github.com>
2025-11-06 16:10:23 -0500

    About screen stuff
    linked about and homesceen, added white backgrounds

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,14 +4,18 @@ using UnityEngine.SceneManagement;
 public class LevelManager : MonoBehaviour
 {
     public string nextLevel;
+
+    //load a new scene based on name
     void LoadSceneByName(string name) {
         SceneManager.LoadScene(name);
     }
 
+    //changes the level name
     public void ChangeNextLevel(string name) {
         nextLevel = name;
     }
 
+    //loads the scene specified by nextLevel
     public void LoadNextLevel()
     {
         if (nextLevel.Length > 0) {
