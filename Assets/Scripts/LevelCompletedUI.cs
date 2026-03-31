40c720ed149bfbeb99b26f26db400b5496004e54
sharon2pan <sharon2.pan@gmail.com>
2026-03-30 21:49:30 -0400

    implement new game over ui

--- Assets/Scripts/LevelCompletedUI.cs
+++ Assets/Scripts/LevelCompletedUI.cs
@@ -1,16 +1,72 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class LevelCompletedUI : MonoBehaviour
 {
     public TextMeshProUGUI factorsScoresText;
     public TextMeshProUGUI totalScoreText;
+    public Slider totalScoreSlider;
+
+    [Header("Client Comments")]
+    public TextMeshProUGUI clientCommentText;
+    public string[] threeStarComments;
+    public string[] twoStarComments;
+    public string[] oneStarComments;
+
+    private int totalStars = 0;
 
     ReputationSystem rs;
 
     void Awake()
     {
         rs = FindFirstObjectByType<ReputationSystem>();
+    }
+
+    public void SetGameOverUI()
+    {
+        SetSlider();
+        SetClientComment();
+    }
+
+    public void SetClientComment()
+    {
+        if (totalStars == 3)
+        {
+            clientCommentText.text = threeStarComments[Random.Range(0, threeStarComments.Length)];
+        }
+        else if (totalStars == 2)
+        {
+            clientCommentText.text = twoStarComments[Random.Range(0, twoStarComments.Length)];
+        }
+        else
+        {
+            clientCommentText.text = oneStarComments[Random.Range(0, oneStarComments.Length)];
+        }
+    }
+
+    public void SetSlider()
+    {
+        // 700 - 1000 => 3 stars
+        // 400 - 699 => 2 stars
+        // 0 - 399 => 1 star
+
+        int totalScore = rs.CalculateTotalReputation();
+        if (totalScore >= 700 && totalScore <= 1000)
+        {
+            totalStars = 3;
+            totalScoreSlider.value = 3;
+        }
+        else if (totalScore >= 400 && totalScore < 700)
+        {
+            totalStars = 2;
+            totalScoreSlider.value = 2;
+        }
+        else
+        {
+            totalStars = 1;
+            totalScoreSlider.value = 1;
+        }
     }
 
     public void ShowScores()
